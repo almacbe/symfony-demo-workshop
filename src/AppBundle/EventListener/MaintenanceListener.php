3b550a405e259c489ce43e4c753c0174aa258b74
Alfonso Machado <almacbe@gmail.com>
2016-12-16 00:09:11 +0100

    add maintenance listener

---
+++ src/AppBundle/EventListener/MaintenanceListener.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AppBundle\EventListener;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+
+class MaintenanceListener
+{
+    /**
+     * @var bool
+     */
+    private $enabled;
+
+    /**
+     * MaintainerListener constructor.
+     */
+    public function __construct($enabled)
+    {
+        $this->enabled = $enabled;
+    }
+
+    /**
+     * @param GetResponseEvent $event
+     */
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        if (!$event->isMasterRequest()) {
+            return;
+        }
+
+        $response = new Response('Estamos en mantenimiento');
+
+        $event->setResponse($response);
+    }
+}
